1202705f0993ec326185025d4d15513d984b831f
Erik <59801674+divinepet@users.noreply.github.com>
2021-12-15 18:12:47 +0500

    Update megaphone.cpp

--- module 00/ex00/megaphone.cpp
+++ module 00/ex00/megaphone.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
-#include <string>
-
-using std::cout;
-using std::string;
-using std::endl;
-using std::toupper;
-
-void print(char c) {
-    cout << c;
-}
 
 int main(int ac, char **av) {
-    if (ac > 1) {
-        while (*++av) {
-            while (*(*av)) {
-                print(toupper(*(*av)++));
-            }
-        }
-    } else
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    endl (cout);
-    return 0;
+	if (ac > 1) {
+		while (*++av)
+			while (*(*av))
+				std::cout << (char)std::toupper(*(*av)++);
+	} else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	endl(std::cout);
+	return 0;
 }
